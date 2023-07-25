4be83e9b214a9edfd0e429254626209c1eeb0db7
Blob2763 <88489444+Blob2763@users.noreply.github.com>
2023-07-25 19:05:37 +0100

    Update pageNav.js

--- pageNav.js
+++ pageNav.js
@@ -1,10 +1,10 @@
 function populateNavbar() {
     const links = {
         "Home": "../index.html",
-        "About": "../about/index.html",
-        "Tools": "../tools/index.html",
-        "Saved": "../saved/index.html",
-        "Settings": "../settings/index.html",
+        "About": "../about",
+        "Tools": "../tools",
+        "Saved": "../saved",
+        "Settings": "../settings",
     }
 
     let content = ""
