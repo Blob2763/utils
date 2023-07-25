e14c25548607fe2f41b49d96d15b2a083b4a62f2
Blob2763 <88489444+Blob2763@users.noreply.github.com>
2023-07-26 00:45:52 +0100

    Add files via upload

--- tools/toolsList.js
+++ tools/toolsList.js
@@ -3,28 +3,58 @@ function getJSON() {
         .then((response) => response.json());
 }
 
-function generateList() {
-    getJSON()
-        .then((toolsJSON) => {
-            toolsJSON = toolsJSON[0]
-            console.log(toolsJSON);
+function generateList(filter) {
+    if (filter === "") {
+        document.getElementById("search-results").style.display = "none"
+        document.getElementById("all-tools").style.display = "inline"
+    } else {
+        document.getElementById("search-results").style.display = "inline"
+        document.getElementById("all-tools").style.display = "none"
 
-            let nameList = [];
+        getJSON()
+            .then((toolsJSON) => {
+                toolsJSON = toolsJSON[0]
+                console.log(toolsJSON);
 
-            for (category in toolsJSON) {
-                console.log(category);
+                let nameList = [];
 
-                let categoryData = toolsJSON[category];
+                for (category in toolsJSON) {
+                    console.log("the category is", category);
 
-                for (tool in categoryData) {
-                    nameList.push(tool);
+                    let categoryData = toolsJSON[category];
+
+                    for (tool in categoryData) {
+                        if (tool.includes(filter.toUpperCase())) {
+                            nameList.push({"tool": tool, "link": categoryData[tool], "category": category});
+                        }
+                    }
                 }
-            }
 
-            console.log(nameList);
-        })
+                console.log(nameList);
 
-        .catch((error) => {
-            console.error('Error fetching tools JSON:', error);
-        });
+                let content = ""
+                for (toolData in nameList) {
+                    console.log(toolData);
+
+                    const tool = nameList[toolData]["tool"];
+                    const link = nameList[toolData]["link"];
+
+                    console.log(tool, link);
+
+                    content = content.concat(`<li><a href="../tools/${category.toLowerCase()}/${link}">${tool} - ${category}</a></li>`)
+                }
+
+                console.log(content);
+
+                if (content === "") {
+                    content = "NO TOOLS FOUND"
+                }
+
+                document.getElementById("results-list").innerHTML = content
+            })
+
+            .catch((error) => {
+                console.error('Error fetching tools JSON:', error);
+            });
+    }
 }
